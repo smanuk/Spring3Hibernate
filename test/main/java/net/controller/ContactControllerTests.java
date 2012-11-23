5683a8ebf8a0fe33c96f66e478c363e7e8dc6a38
Steven Manikiam <sman@manikiam.net>
2012-11-23 17:00:53 +0000

    Started added further tests.
    Attempting to use the full functionality of branching,editing and
    merging

---
+++ test/main/java/net/controller/ContactControllerTests.java
@@ -0,0 +1,16 @@
+package net.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ContactControllerTests
+{
+
+}
